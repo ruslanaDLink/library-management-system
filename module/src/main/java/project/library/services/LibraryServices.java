73a8183b43fa4b0d9b99c8d8a9e484a6f03270a9
Ruslana Babiak <ruslana.babiak@gmail.com>
2024-02-18 13:28:36 +0100

    Added library services.

--- module/src/main/java/project/library/services/LibraryServices.java
+++ module/src/main/java/project/library/services/LibraryServices.java
@@ -1,4 +1,15 @@
 package project.library.services;
 
+import project.library.model.User;
+
 public class LibraryServices {
+
+    public void registerUser(User user) {
+
+    }
+
+    private int generatePin() {
+        return 0;
+    }
+
 }
