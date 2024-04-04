4fc3c94072314ea048e9dcd56d7c2feb75b27080
Ruslana Babiak <ruslana.babiak@gmail.com>
2024-04-04 07:58:13 +0200

    Added user interface with implementation class.

---
+++ module/src/main/java/project/library/user/UserInterface.java
@@ -0,0 +1,21 @@
+package project.library.user;
+
+import project.library.exception.BookNotFoundException;
+import project.library.model.Book;
+
+interface UserInterface {
+
+    void add(Book book) throws BookNotFoundException;
+
+    void remove(Book book);
+
+    void borrow(Book book);
+
+    Book searchByTitle(String title);
+
+    Book searchByAuthor(String author);
+
+    Book searchByGenre(String genre);
+
+    void returnBack(Book book);
+}
